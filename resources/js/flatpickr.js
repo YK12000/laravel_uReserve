d9494fc71e00be934d3d6ff39cd503a6a41e8744
yk12000 <6279020+YK12000@users.noreply.github.com>
2022-04-03 09:39:01 +0900

    event dummy 修正

--- resources/js/flatpickr.js
+++ resources/js/flatpickr.js
@@ -7,14 +7,22 @@ flatpickr("#event_date", {
     maxDate: new Date().fp_incr(30)
 });
 
+flatpickr("#calendar", {
+    "locale": Japanese,
+    // minDate: "today",
+    maxDate: new Date().fp_incr(30)
+});
+
+
 const setting = {
     "locale": Japanese,
     enableTime: true,
     noCalendar: true,
     dateFormat: "H:i",
     time_24hr: true,
-    minTime: "16:00",
-    maxTime: "22:30",
+    minTime: "10:00",
+    maxTime: "20:00",
+    minuteIncrement
 }
 
 flatpickr("#start_time", setting);
